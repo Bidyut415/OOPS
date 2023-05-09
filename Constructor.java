0364f858e93f83127a3510914e9872809a7ff9c9
Bidyut415 <wdevlopment415@gmail.com>
2023-05-09 15:30:29 +0530

    Constructor file add

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+class student{
+    
+    Student(){
+        // this is constructor without paremetar . it extiquton first 
+        System.out.println("when the obj creat at that moment the constructor called");
+    }
+}
+
+public class Constructor {
+
+    public static void main(String[] args) {
+        Student s1=new Student();
+    }
+}
