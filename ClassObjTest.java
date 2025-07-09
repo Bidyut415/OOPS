7b70ed953535d1c6d8d179138f9354f7c08369e4
Bidyut Majhi <wdevlopment415@gmail.com>
2025-07-10 02:18:32 +0530

    Test Class and Obj

---
+++ ClassObjTest.java
@@ -0,0 +1,8 @@
+public class ClassObjTest {
+  int x=5;
+  public static void main(String[] args) {
+    ClassObjTest myobj =new ClassObjTest();//obj creation
+    System.out.println(myobj.x);//fetch the obj
+  }
+}
+
