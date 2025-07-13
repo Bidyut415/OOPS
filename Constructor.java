6698539ececc34ab3e50e650aac5c3f410f8d27a
Bidyut Majhi <wdevlopment415@gmail.com>
2025-07-13 22:14:33 +0530

    add new java file Constructor

---
+++ Constructor.java
@@ -0,0 +1,11 @@
+/*CONSTRUCTOR */
+class Student{
+  Student(){
+    System.out.println("i am Default Constructor");
+  }
+}
+public class Constructor {
+  public static void main(String[] args) {
+    Student s1=new Student();//Automatically call
+  }
+}
